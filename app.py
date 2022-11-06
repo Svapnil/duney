e237ca497972db7c9f0e1eb308e35b2d736f6781
Svapnil <svapnil@Svapnils-MacBook-Pro.local>
2022-11-06 00:34:18 -0700

    commented out redis info

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from dune import execute_query, get_query_results, get_ens_query_execution_id
-import redis
+# import redis
 import os
 
 app = Flask(__name__)
-redis = redis.StrictRedis(host='containers-us-west-86.railway.app', password=os.getenv("REDIS_PASSWORD"), port='6624', charset="utf-8", decode_responses=True)
+# redis = redis.StrictRedis(host='containers-us-west-86.railway.app', password=os.getenv("REDIS_PASSWORD"), port='6624', charset="utf-8", decode_responses=True)
 
 @app.route("/")
 def hello_world():
@@ -25,29 +25,25 @@ def hello_world():
         </div>
         """ % (final_result["address"], final_result["name"], final_result["latest_tx"])
 
-@app.route('/<ens_domain>')
-def get_ens_info(ens_domain):
-#   exec_id = get_ens_query_execution_id(ens_domain)
+# @app.route('/<ens_domain>')
+# def get_ens_info(ens_domain):
 
-  # Have we already run this query?
-  if redis.exists(f"{ens_domain}.exec_id"):
-    exec_id = redis.get(f"{ens_domain}.exec_id")
-    r = get_query_results(exec_id)
-    if (r.json()["state"] == "QUERY_STATE_EXECUTING"):
-        return "We are still executing the query for %s.eth" % ens_domain
-    else:
-        return build_result_output(r)
-  else:
-    exec_id = get_ens_query_execution_id(ens_domain)
-    redis.set(f"{ens_domain}.exec_id", exec_id)
-    return """
-      <div>
-        We are executing your ENS query now for the %s.eth username! Check back in a minute or two for the result!
-      </div>
-    """ % ens_domain
-#   return """
-#     <div>%s</div>
-#   """ % ens_domain
+#   # Have we already run this query?
+#   if redis.exists(f"{ens_domain}.exec_id"):
+#     exec_id = redis.get(f"{ens_domain}.exec_id")
+#     r = get_query_results(exec_id)
+#     if (r.json()["state"] == "QUERY_STATE_EXECUTING"):
+#         return "We are still executing the query for %s.eth" % ens_domain
+#     else:
+#         return build_result_output(r)
+#   else:
+#     exec_id = get_ens_query_execution_id(ens_domain)
+#     redis.set(f"{ens_domain}.exec_id", exec_id)
+#     return """
+#       <div>
+#         We are executing your ENS query now for the %s.eth username! Check back in a minute or two for the result!
+#       </div>
+#     """ % ens_domain
 
 def build_result_output(response):
     print(response.json())
